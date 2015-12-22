42fb839dcf1c8d50eedb2430615a9d4d84a29b89
neofenril <arthurdu88@hotmail.com>
2015-12-22 18:21:26 +0100

    Gestion forum abonnement
    Ajout d'une fonciton pas encore testée pour rechercher les forums ou
    l'utilisateur est abonné et dont la date est inférieure à la date
    actuelle
    Du coup j'ai fais deux trois test et ca a changer un peu dans le fichier
    save.log, ca confirme que ca marche julien ,;)

--- abonnement.py
+++ abonnement.py
@@ -3,6 +3,9 @@
 # -*- coding: utf8 -*-
 
 from base import Base
+import time
+ts=time.time()
+
 
 class Abonnement:
 
@@ -48,3 +51,26 @@ class Abonnement:
 			
 		db.close()
 		return res
+
+
+"""  Ici c'est la fonction qui va récuperer la liste des forums ou l'utilisateur possède des messages non lus
+	Sachant qu'un forum dont le message non lu est un forum qui se trouve dans abonnement et dont la date dans last_visite est inférieur à la date actuelle.
+	utiliser la variable ts définie plus haut pour récupérer le temps actuel qui sera à comparer avec la date de l'utilisateur.
+
+	def getAllForumAboNonLus(idu):
+		base=Base()
+		db=base.connection()
+		cursor=db.cursor()
+		query="SELECT id_forum from abonnement WHERE id_utilisateur="+idu+" AND last_visite<getdate"
+		lines = cursor.execute(query)
+		res=[]
+		i=0
+		while True:
+			row=cursor.fetchone()
+			if row=None:
+				break
+			res[i] = row
+
+		db.close()
+		return res
+		"""
