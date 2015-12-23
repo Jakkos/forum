aafb0959ae94460f51cbf60345132ea84c26c6e4
Jakkos <julien.remy88@orange.fr>
2015-12-23 18:32:49 +0100

    ajout file message operation
    tout ne fonction peut etre pas, mais c'est mis en place
    j'utilise toujours les threads pour envoi de message vers client

--- servforum.py
+++ servforum.py
@@ -10,19 +10,9 @@ import sys
 import threading
 import time
 import datetime
+import posix_ipc
 
-ts = time.time()
-
-
-dateMoinsUneHeure = datetime.date.today() - datetime.timedelta(hours=1)
-dateMoinsUneHeureTimestamp = dateMoinsUneHeure.strftime("%s")
-
-date1 = datetime.date.today()
-date2 = datetime.date.today() - datetime.timedelta(days=1)
-
-
-
-
+#configuration des logs
 import logging
 formatter = logging.Formatter("%(asctime)s -- %(name)s -- %(levelname)s -- %(message)s")
 handler_file = logging.FileHandler("save.log",mode = "a",encoding="utf-8")
@@ -32,6 +22,16 @@ logger = logging.getLogger("servforum")
 logger.setLevel(logging.INFO)
 logger.addHandler(handler_file)
 
+#gestion des dates
+ts = time.time()
+
+dateMoinsUneHeure = datetime.date.today() - datetime.timedelta(hours=1)
+dateMoinsUneHeureTimestamp = dateMoinsUneHeure.strftime("%s")
+
+date1 = datetime.date.today()
+date2 = datetime.date.today() - datetime.timedelta(days=1)
+
+#import des fonctions de base de donnees
 from base import Base
 from unforum import UnForum
 
@@ -45,8 +45,17 @@ class ThreadClient(threading.Thread):
 	#dialogue avec le client
 		nom=self.getName()
 		while 1:
-			msgClient=self.connexion.recv(1024).decode("Utf8")
-			# if msgClient.upper() == "CREATE TABLE\n":
+			# file
+			try:
+				mq = posix_ipc.MessageQueue("operation_file",posix_ipc.O_CREX)
+				logger.info("Creation de la file d'operation")
+			except posix_ipc.ExistentialError:
+				mq = posix_ipc.MessageQueue("operation_file",posix_ipc.O_CREAT)
+			operation = mq.receive()
+			message = operation[0]
+			priority = operation[1]
+			# message=self.connexion.recv(1024).decode("Utf8")
+			# if message.upper() == "CREATE TABLE\n":
 			# 	query0 = "CREATE TABLE forum(id_forum INTEGER PRIMARY KEY AUTOINCREMENT unique,theme CHAR(50) NOT NULL);"
 			# 	query1 = "CREATE TABLE utilisateur(id_utilisateur INTEGER PRIMARY KEY AUTOINCREMENT unique,pseudo CHAR(50) NOT NULL,password CHAR(25) NOT NULL);"
 			# 	query2 = "CREATE TABLE message(id_message INTEGER PRIMARY KEY AUTOINCREMENT unique,id_utilisateur INTEGER NOT NULL,id_forum INTEGER NOT NULL,texte CHAR(50) NOT NULL,date_message DATETIME NOT NULL,FOREIGN KEY(id_utilisateur) REFERENCES utilisateur(id_utilisateur)FOREIGN KEY(id_forum) REFERENCES forum(id_forum));"
@@ -65,56 +74,65 @@ class ThreadClient(threading.Thread):
 			for cle in conn_client:
 				 if cle == nom:
 				 	client = conn_client[cle]
-			if msgClient.upper() == "INSERT FORUM\n":
-				database = Base()
-				db = database.connection()
-				cursor = db.cursor()
-				query = "INSERT INTO forum(theme) VALUES('Deuxieme forum')"
-				cursor.execute(query)
-				db.commit()
-			elif msgClient.upper() == "INSERT USER\n":
-				database = Base()
-				db = database.connection()
-				cursor = db.cursor()
-				query = "INSERT INTO utilisateur(pseudo,password) VALUES('admin','admin')"
-				cursor.execute(query)
-				db.commit()
-			elif msgClient.upper() == "ALL FORUMS\n":
-				database = Base()
-				db = database.connection()
-				cursor = db.cursor()
-				query = "SELECT * FROM forum"
-				cursor.execute(query)
-				for i in cursor:
-					client.send(i[1].encode("Utf8"))
-			elif msgClient.upper() == "ALL USERS\n":
-				database = Base()
-				db = database.connection()
-				cursor = db.cursor()
-				query = "SELECT * FROM utilisateur"
-				cursor.execute(query)
-				for i in cursor:
-					print i
+			# if message.upper() == "INSERT FORUM\n":
+			# 	database = Base()
+			# 	db = database.connection()
+			# 	cursor = db.cursor()
+			# 	query = "INSERT INTO forum(theme) VALUES('Deuxieme forum')"
+			# 	cursor.execute(query)
+			# 	db.commit()
+			# elif message.upper() == "INSERT USER\n":
+			# 	database = Base()
+			# 	db = database.connection()
+			# 	cursor = db.cursor()
+			# 	query = "INSERT INTO utilisateur(pseudo,password) VALUES('admin','admin')"
+			# 	cursor.execute(query)
+			# 	db.commit()
+			# elif message.upper() == "ALL FORUMS\n":
+			# 	database = Base()
+			# 	db = database.connection()
+			# 	cursor = db.cursor()
+			# 	query = "SELECT * FROM forum"
+			# 	cursor.execute(query)
+			# 	for i in cursor:
+			# 		client.send(i[1].encode("Utf8"))
+			# elif message.upper() == "ALL USERS\n":
+			# 	database = Base()
+			# 	db = database.connection()
+			# 	cursor = db.cursor()
+			# 	query = "SELECT * FROM utilisateur"
+			# 	cursor.execute(query)
+			# 	for i in cursor:
+			# 		print i
 			# 1- Creer un nouveau forum
-			elif msgClient == "1\n":
+			if message == "1":
 				msg = "Entrer le theme de votre forum en respectant la synthaxe suivante (newf>La jardinerie c'est ma passion !)\n"
+				# c'est quoi ces print ?
 				print date1
 				print date2
 				logger.info("tentative creation forum")
 				client.send(msg.encode("Utf8"))
+				try:
+					mq = posix_ipc.MessageQueue("operation_file",posix_ipc.O_CREX)
+					logger.info("Creation de la file d'operation")
+				except posix_ipc.ExistentialError:
+					mq = posix_ipc.MessageQueue("operation_file",posix_ipc.O_CREAT)
+				operation = mq.receive()
+				message = operation[0]
+				priority = operation[1]
 			# newf>
-			elif msgClient[0:5] == "newf>":
-				if len(msgClient)<12:
+			elif message[0:5] == "newf>":
+				if len(message)<12:
 					logger.warning("probleme creation forum (titre)")
 					msg = "Erreur, le theme du forum doit contenir au moins 5 caracteres !\nRessayer :"
 				else:
 					forum = UnForum()
-					forum.insert(msgClient[5:len(msgClient)-1])
-					logger.info("creation forum : %s",msgClient[5:len(msgClient)-1])
+					forum.insert(message[5:len(message)-1])
+					logger.info("creation forum : %s",message[5:len(message)-1])
 					msg = "Forum cree !\n\nMenu :\n1- Creer un nouveau forum\n2- Afficher tous les forums\n3- Afficher les forums preferes\n4- Se deconnecter\n\nEntrer 1,2,3 ou 4 :"
 				client.send(msg.encode("Utf8"))
 			# 2- Afficher tous les forums
-			elif msgClient == "2\n":
+			elif message == "2":
 				database = Base()
 				db = database.connection()
 				cursor = db.cursor()
@@ -122,16 +140,18 @@ class ThreadClient(threading.Thread):
 				cursor.execute(query)
 				for i in cursor:
 					client.send(i[1].encode("Utf8"))
+				logger.info("recuperation de la liste des forums par : %s",message[5:len(message)-1])
 			# 4- Se deconnecter
-			elif not msgClient or msgClient == "4\n":
+			elif not message or message == "4":
+				# la aussi il faut couper la connection plus correctement
 				break
 
-			message="%s> %s" % (nom,msgClient)
+			message="%s> %s" % (nom,message)
 			print(message)
-			# #faire suivre le message aux autres clients
-			# for cle in conn_client:
-			# 	if cle == nom:
-			# 		conn_client[cle].send(message.encode("Utf8"))
+			#faire suivre le message aux autres clients
+			for cle in conn_client:
+				if cle == nom:
+					conn_client[cle].send(message.encode("Utf8"))
 # Fermeture de la connexion
 		self.connexion.close()
 		del conn_client[nom]
